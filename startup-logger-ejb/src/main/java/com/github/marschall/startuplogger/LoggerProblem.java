3cbb6807e9cbdcf8215eb038e83378e63c6eca80
Philippe Marschall <philippe.marschall@gmail.com>
2013-10-12 11:40:54 +0200

    bump ejb

---
+++ startup-logger-ejb/src/main/java/com/github/marschall/startuplogger/LoggerProblem.java
@@ -0,0 +1,23 @@
+package com.github.marschall.startuplogger;
+
+import java.lang.invoke.MethodHandles;
+
+import javax.ejb.Schedule;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Singleton
+@Startup
+public class LoggerProblem {
+
+    private static final Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+    @Schedule(second = "*/10", persistent = false)
+    public void log() {
+        LOG.info("ok");
+    }
+
+}
